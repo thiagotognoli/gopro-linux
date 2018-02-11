1bbbb672886ecc306187eb95fb5088a8f19defef
Thiago Tognoli <thiagotognoli@users.noreply.github.com>
2018-02-11 12:05:15 -0200

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 videosList="";
 for video in $(ls *.MP4|sort -n); do
-  videosList="$videosList -cat $file";
+  videosList="$videosList -cat $video";
 done;
 MP4Box $videosList -new onefile.mp4;
 
